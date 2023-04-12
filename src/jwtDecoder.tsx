b6cf929410f1edc580aa51c0c5e58be2da3d2efe
nologyance <nologyance>
2023-04-12 19:36:46 +0900

    Format datetime value

--- src/jwtDecoder.tsx
+++ src/jwtDecoder.tsx
@@ -4,12 +4,19 @@ import { Action, ActionPanel, Form, showToast, Toast } from '@raycast/api';
 export default function Command() {
   const [jwt, setJwt] = useState('');
   const [decoded, setDecoded] = useState('');
+  const [issuedAt, setIssuedAt] = useState('');
+  const [expiredAt, setExpiredAt] = useState('');
 
   const handleSubmit = () => {
     try {
       const [header, payload, signature] = jwt.split('.');
       const decodedPayload = Buffer.from(payload, 'base64').toString('utf-8');
-      const decodedString = JSON.stringify(JSON.parse(decodedPayload), null, 2);
+      const parsedPayload = JSON.parse(decodedPayload);
+      const issuedAt = new Date(parsedPayload.iat).toLocaleString('ja-JP');
+      const expiredAt = new Date(parsedPayload.exp).toLocaleString('ja-JP');
+      const decodedString = JSON.stringify(parsedPayload, null, 2);
+      setIssuedAt(issuedAt);
+      setExpiredAt(expiredAt);
       setDecoded(decodedString);
     } catch (error: any) {
       showToast({
@@ -27,8 +34,10 @@ export default function Command() {
           <Action.SubmitForm title="Decode" onSubmit={handleSubmit} />
         </ActionPanel>
       }>
-      <Form.TextField id="jwt" value={jwt} onChange={setJwt} />
-      <Form.TextArea id="decoded" value={decoded} />
+      <Form.TextField id="jwt" title="Payload" value={jwt} onChange={setJwt} />
+      <Form.TextArea id="decoded" title="decoded" value={decoded} />
+      <Form.TextField id="issued_at" title="issued_at" value={issuedAt} />
+      <Form.TextField id="expired_at" title="expired_at" value={expiredAt} />
     </Form>
   );
 }
